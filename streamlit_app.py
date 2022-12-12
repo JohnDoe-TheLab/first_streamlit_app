b06de8bf50e6a069ab3b933593afa3681ef8518a
JohnDoe-TheLab <120416869+JohnDoe-TheLab@users.noreply.github.com>
2022-12-12 14:08:32 -0600

    Update streamlit_app.py
    Added more items and improved format

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,12 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breadfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled Free Range Egg')
+streamlit.header('Breadfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
